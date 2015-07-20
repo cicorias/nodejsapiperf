526e09fd0fcd601f67d5b27709865b7accd138f8
Shawn Cicoria <shawn@cicoria.com>
2015-07-20 16:16:33 -0400

    updates

--- src/MainWeb/ExpressApp1/app.js
+++ src/MainWeb/ExpressApp1/app.js
@@ -1,63 +1,149 @@
-﻿var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require( './routes/users' );
-var values = require( './routes/values.js' );
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use( '/users', users );
-app.use( '/values', values );
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+﻿if ( process.env.new_relic_app_name )
+    require( 'newrelic' );
 
 
-module.exports = app;
+var express = require( 'express' );
+
+global.cookieLifetime = 1000 * 60 * 60 * 24 * 7;
+
+global._ = require( 'lodash' );
+//global.loader = require( require( 'path' ).join( __dirname, 'lib', 'loader' ) );
+//global.log = require( 'log' );// loader.lib( 'log' );
+
+var Log = require( 'log' ),
+  log = new Log( 'info' );
+
+var app = express( );
+
+app.get( '/dummy', function ( req, res, next ) {
+    res.send( { 'ido': true } );
+} );
+
+var apiKey = "foobar";//loader.config( ).apiKey;
+var https = require( 'https' );
+
+https.globalAgent.keepAlive = true;
+https.globalAgent.maxSockets = 500;
+
+
+app.get( '/count', function ( req, res, next ) {
+    var count = 0;
+    var total = 10000;
+    var began = ( new Date( ) ).valueOf( );
+    var next = function ( i ) {
+        if ( i > total )
+            return;
+        //res.write('loading:' + i + '\n');
+        var start = ( new Date( ) ).valueOf( );
+        https.request( {
+            'hostname': 'APIHOST.com',
+            'path': '/api/Account/ClientTicket',
+            'headers': {
+                'APIKey': apiKey,
+                'Connection': 'Keep-Alive'
+            }
+        }, function ( response ) {
+            response.on( 'data', function () { } );
+            response.on( 'end', function () {
+                var end = ( new Date( ) ).valueOf( );
+                count++;
+                res.write( 'loaded:' + i + ' - ' + count + '  ' + res.statusCode + ' ' + ( end - start ) + 'ms!\n' );
+                if ( count == total ) {
+                    var end = ( new Date( ) ).valueOf( );
+                    res.write( 'Completed in ' + ( end - began ) + 'ms.' );
+                    return res.end( );
+                }
+            } );
+            response.on( 'error', function () {
+                var end = ( new Date( ) ).valueOf( );
+                count++;
+                res.write( 'ERROR:' + i + ' - ' + count + '  ' + res.statusCode + ' ' + ( end - start ) + 'ms!\n' );
+                if ( count == total ) {
+                    var end = ( new Date( ) ).valueOf( );
+                    res.write( 'Completed in ' + ( end - began ) + 'ms.' );
+                    return res.end( );
+                }
+            } );
+        } ).end( );
+        
+        setTimeout( function () {
+            next( i + 1 );
+        }, 0 );
+    };
+    
+    next( 0 );
+} );
+
+app.get( '/search_test', function ( req, res, next ) {
+    getClientTicket( function () {
+        res.json( { 'success': true } );
+    } );
+} );
+
+var getClientTicket = function ( cb ) {
+    https.request( {
+        'hostname': 'APIHOST.com',
+        'path': '/api/Account/ClientTicket',
+        'headers': {
+            'APIKey': apiKey
+        }
+    }, function ( response ) {
+        var res = '';
+        response.on( 'data', function ( chunk ) {
+            res += chunk;
+        } );
+        
+        response.on( 'error', function () {
+            cb( );
+        } );
+        
+        response.on( 'end', function () {
+            try {
+                res = JSON.parse( res );
+            } catch ( e ) {
+                res = {};
+            }
+            cb( res.ticket );
+        } );
+    } ).end( );
+};
+
+var qs = require( 'querystring' );
+
+app.get( '/search_test_2', function ( req, res, next ) {
+    getClientTicket( function ( ticket ) {
+        https.request( {
+            'hostname': 'APIHOST.com',
+            'path': '/api/products/search?' + qs.stringify( {
+                'searchText': 'huggies',
+                'from': 0,
+                'size': 24,
+                'zipCode': '07843'
+            } ),
+            'headers': {
+                'APIKey': apiKey,
+                'ClientTicket': ticket,
+                'Accept': 'application/vnd.apihost.productsearchresult.v1+json'
+            }
+        }, function ( response ) {
+            response.on( 'data', function ( chunk ) { } );
+            response.on( 'end', function () {
+                res.json( { 'success': true } );
+            } );
+        } ).end( );
+    } );
+} );
+
+
+var port = 8888;//loader.lib( 'args' ).port;
+
+var server = app.listen( port, function () {
+    log.info( 'Express web running on port:', port );
+} );
+
+process.on( 'uncaughtException', function ( err ) {
+
+    log.error( 'Uncaught Exception', err.code, err.message, err.stack );
+} );
+
+
