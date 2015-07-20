e8022a63f8e9353fb9dd87ed75bee746a60cd6f6
Shawn Cicoria <shawn@cicoria.com>
2015-07-20 13:19:33 -0400

    added sites - inital

---
+++ src/MainWeb/ExpressApp1/routes/values.js
@@ -0,0 +1,78 @@
+﻿var express = require( 'express' );
+var router = express.Router( );
+var http = require( 'http' );
+
+var options = {
+    host: 'jettestarmpaas.cloudapp.net',
+    port: 80,
+    path: '/api/simple',
+    method: 'GET',
+    headers: {
+        connection: 'keep-alive'
+    }
+};
+
+
+function callApi( options, callback ) {
+    return http.request( options, function ( res ) {
+        console.log( 'STATUS: ' + res.statusCode );
+        console.log( 'HEADERS: ' + JSON.stringify( res.headers ) );
+        res.setEncoding( 'utf8' );
+        
+        var body = '';
+        res.on( 'data', function ( chunk ) {
+            console.log( 'BODY: ' + chunk );
+            body += chunk;
+        } );
+        
+        res.on( 'end', function ( foo ) {
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse( body );
+            
+            var st = res.status;
+            if ( res.statusCode == 200 )
+                callback( null, parsed );
+            else {
+                callback( res.statusCode, parsed );
+            }
+
+        } );
+
+    } ).on( 'error', function ( e ) {
+        callback( true, "error" );
+
+    } ).on( 'timeout', function ( e ) {
+        res.abort( );
+        callback( true, "timeout" );
+    } );
+    
+    
+}
+
+
+/* GET values page. */
+router.get( '/', function ( req, res ) {
+    var stuff, rawData;
+    callApi( options, function ( err, data ) {
+        
+        if ( err ) {
+            res.status( err );
+            var currentStack = new Error( ).stack;
+            stuff = JSON.stringify( data );
+            res.render( 'error', { message: stuff, error: { status: err, stack: currentStack }} );
+        }
+        else {
+            stuff = JSON.stringify( data );
+            rawData = data;
+            res.render( 'values', { title: 'Values', body: stuff, data: rawData } );
+        }
+
+    } ).end( );
+    
+
+
+} );
+
+module.exports = router;
+
+
---
+++ src/MainWeb/MainWeb/Controllers/SimpleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace MainWeb.Controllers
+{
+    [OutputCache(Location = OutputCacheLocation.Server)]
+    public class SimpleController : ApiController
+    {
+
+        [OutputCache(Location=OutputCacheLocation.Server)]
+        // GET: api/Simple
+        public IEnumerable<string> Get()
+        {
+            var length = 5;
+            var rv = new string[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                rv[i] = DateTime.UtcNow.ToString("s");    
+            }
+
+            return rv;
+        }
+
+        //// GET: api/Simple/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Simple
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Simple/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Simple/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
