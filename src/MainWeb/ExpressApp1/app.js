1e36741eb7b2166b9859f00868c0939210023bc2
Shawn Cicoria <shawn@cicoria.com>
2015-07-21 13:28:39 -0400

    updates

--- src/MainWeb/ExpressApp1/app.js
+++ src/MainWeb/ExpressApp1/app.js
@@ -1,10 +1,9 @@
-﻿
+﻿var express = require( 'express' );
+var Log = require( 'log' ),
+    log = new Log( 'info' );
 
-if ( process.env.new_relic_app_name )
-    require( 'newrelic' );
+var app = express( );
 
-
-var express = require( 'express' );
 
 global.cookieLifetime = 1000 * 60 * 60 * 24 * 7;
 
@@ -12,20 +11,25 @@ global._ = require( 'lodash' );
 //global.loader = require( require( 'path' ).join( __dirname, 'lib', 'loader' ) );
 //global.log = require( 'log' );// loader.lib( 'log' );
 
-var Log = require( 'log' ),
-  log = new Log( 'info' );
 
-var app = express( );
+app.get( '/', function ( req, res, next ) {
+    res.send( { 'working' : 'true' } );
+} );
+
+
 
 app.get( '/dummy', function ( req, res, next ) {
     res.send( { 'ido': true } );
 } );
 
 var apiKey = "foobar";//loader.config( ).apiKey;
-var https = require( 'https' );
+var apiKey = 'e9d5e5d5-5862-496e-a5fb-af0f8d90b0b0';
 
-https.globalAgent.keepAlive = true;
-https.globalAgent.maxSockets = 500;
+var https = require( 'https' );
+var http = require( 'http' );
+
+http.globalAgent.keepAlive = true;
+http.globalAgent.maxSockets = 500;
 
 
 app.get( '/count', function ( req, res, next ) {
@@ -37,8 +41,8 @@ app.get( '/count', function ( req, res, next ) {
             return;
         //res.write('loading:' + i + '\n');
         var start = ( new Date( ) ).valueOf( );
-        https.request( {
-            'hostname': 'APIHOST.com',
+        http.request( {
+            'hostname': 'jettestarmpaas.cloudapp.net',
             'path': '/api/Account/ClientTicket',
             'headers': {
                 'APIKey': apiKey,
@@ -76,18 +80,19 @@ app.get( '/count', function ( req, res, next ) {
     next( 0 );
 } );
 
-app.get( '/search_test', function ( req, res, next ) {
-    getClientTicket( function () {
-        res.json( { 'success': true } );
+
+app.get( '/search_fake', function ( req, res, next ) {
+    getClientTicketFake( function ( data ) {
+        res.json( { 'success': true , 'data': data } );
     } );
 } );
 
-var getClientTicket = function ( cb ) {
-    https.request( {
-        'hostname': 'APIHOST.com',
+var getClientTicketFake = function ( cb ) {
+    http.request( {
+        'hostname': 'jettestarmpaas.cloudapp.net',
         'path': '/api/Account/ClientTicket',
         'headers': {
-            'APIKey': apiKey
+            'JetAPIKey': apiKey
         }
     }, function ( response ) {
         var res = '';
@@ -103,7 +108,43 @@ var getClientTicket = function ( cb ) {
             try {
                 res = JSON.parse( res );
             } catch ( e ) {
-                res = { ticket:e.message};
+                res = { ticket: e.message };
+            }
+            cb( res.ticket );
+        } );
+    } ).end( );
+};
+
+
+
+app.get( '/search_test', function ( req, res, next ) {
+    getClientTicket( function ( data ) {
+        res.json( { 'success': true , 'data': data } );
+    } );
+} );
+
+var getClientTicket = function ( cb ) {
+    https.request( {
+        'hostname': 'jettestarmpaas.cloudapp.net',
+        'path': '/api/Account/ClientTicket',
+        'headers': {
+            'JetAPIKey': apiKey
+        }
+    }, function ( response ) {
+        var res = '';
+        response.on( 'data', function ( chunk ) {
+            res += chunk;
+        } );
+        
+        response.on( 'error', function () {
+            cb( );
+        } );
+        
+        response.on( 'end', function () {
+            try {
+                res = JSON.parse( res );
+            } catch ( e ) {
+                res = { ticket: e.message };
             }
             cb( res.ticket );
         } );
@@ -115,7 +156,7 @@ var qs = require( 'querystring' );
 app.get( '/search_test_2', function ( req, res, next ) {
     getClientTicket( function ( ticket ) {
         https.request( {
-            'hostname': 'APIHOST.com',
+            'hostname': 'jettestarmpaas.cloudapp.net',
             'path': '/api/products/search?' + qs.stringify( {
                 'searchText': 'huggies',
                 'from': 0,
@@ -125,7 +166,7 @@ app.get( '/search_test_2', function ( req, res, next ) {
             'headers': {
                 'APIKey': apiKey,
                 'ClientTicket': ticket,
-                'Accept': 'application/vnd.apihost.productsearchresult.v1+json'
+                'Accept': 'application/vnd.jet.productsearchresult.v1+json'
             }
         }, function ( response ) {
             response.on( 'data', function ( chunk ) { } );
@@ -137,15 +178,32 @@ app.get( '/search_test_2', function ( req, res, next ) {
 } );
 
 
-var port = 8888;//loader.lib( 'args' ).port;
+// error handlers
 
-var server = app.listen( port, function () {
-    log.info( 'Express web running on port:', port );
+// development error handler
+// will print stacktrace
+app.use( function ( err, req, res, next ) {
+    res.status( err.status || 500 );
+    res.render( 'error', {
+        message: err.message,
+        error: err
+    } );
 } );
+
+
+
+//var port = 8888;//loader.lib( 'args' ).port;
+//app.set( 'port', process.env.PORT || 3000 );
+
+//var server = app.listen( app.get( 'port' ), function () {
+//    log.info( 'Express web running on port:', app.get( 'port' ) );
+//} );
+
 
 process.on( 'uncaughtException', function ( err ) {
-
-    log.error( 'Uncaught Exception', err.code, err.message, err.stack );
+    //log.error( 'Uncaught Exception', err.code, err.message, err.stack );
 } );
 
+
+module.exports = app;
 
---
+++ src/MainWeb/MainWeb/Controllers/ClientTicketController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace MainWeb.Controllers
+{
+    public class ClientTicketController : ApiController
+    {
+
+        [AllowAnonymous]
+        [Route("api/account/ClientTicket")]
+        // GET: api/ClientTicket
+        public HttpResponseMessage Get()
+        {
+            var clientid = "foobar" + Guid.NewGuid().ToString("N");
+            var ticketdata = Convert.ToBase64String( Encoding.UTF8.GetBytes(clientid));
+            var ticketResponse = new { ticket = ticketdata };
+            return Request.CreateResponse(ticketResponse);
+        }
+
+        //// GET: api/ClientTicket/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/ClientTicket
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/ClientTicket/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/ClientTicket/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
